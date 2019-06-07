3b3e68b525dba82b19435946a5ea98b4340d6fd5
Victor <victor.ossa@hotmail.com>
2019-06-07 11:06:28 -0500

    Agregar archivos de proyecto.

---
+++ CnxCodeFirst/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CnxCodeFirst
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CnxCodeFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CnxCodeFirst.MvcApplication" Language="C#" %>
